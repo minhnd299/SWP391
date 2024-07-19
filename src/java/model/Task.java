80e64d4f420444e5c4a07cfabc1db2108f88c0bc
Nguyễn Khánh Duy <duynkhe163378@fpt.edu.vn>
2024-07-19 09:50:08 +0700

    commit (cont)

--- src/java/model/Task.java
+++ src/java/model/Task.java
@@ -4,6 +4,7 @@
  */
 package model;
 
+import java.sql.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +18,10 @@ public class Task {
     private String description;
     private String status;
     private Objective objecttive;
-
+    private Date start_date;
+    private Date end_date;
+    private float grade;
+    private String link_code;
     public Task(String title, String description, String status, Objective objecttive) {
         this.title = title;
         this.description = description;
